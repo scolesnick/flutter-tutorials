bd90b16cd2d617c19ba0e505487169872cc5fe93
Nick Scoles <scolesnick@gmail.com>
2024-01-13 20:20:42 -0600

    Finished section 7.

--- starting_setup/lib/main.dart
+++ starting_setup/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:starting_setup/keys/keys.dart';
 import 'ui_updates_demo.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter Internals'),
         ),
-        body: const UIUpdatesDemo(),
+        body: const Keys(),
       ),
     );
   }
